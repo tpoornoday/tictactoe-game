f29083254567aaf9a55935881b995c48e2a3c523
Tiruveedi Poornoday <tpoornoday01@gmail.com>
2024-02-29 11:05:01 +0530

    display draw message. apply styles for status message

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,45 @@
+// eslint-disable-next-line react/prop-types
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  // eslint-disable-next-line react/prop-types
+  const noMovesLeft = squares.every(squareValue => squareValue !== null);
+  const nextPlayer = isXNext ? 'X' : 'O';
+
+  const renderStatusMessage = () => {
+    if (winner) {
+      return (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      );
+    }
+
+    if (!winner && noMovesLeft) {
+      return (
+        <>
+          <span className="text-orange">O</span> and{' '}
+          <span className="text-green">X</span> Draw
+        </>
+      );
+    }
+
+    if (!winner && !noMovesLeft) {
+      return (
+        <>
+          Next player is{' '}
+          <span className={isXNext ? 'text-green' : 'text-orange'}>
+            {nextPlayer}
+          </span>
+        </>
+      );
+    }
+
+    return null;
+  };
+
+  return <h2 className="status-message">{renderStatusMessage()}</h2>;
+};
+
+export default StatusMessage;
