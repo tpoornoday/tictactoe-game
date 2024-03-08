c83909f296aab669a8200b7ce4d69d0c63e0aa61
Tiruveedi Poornoday <tpoornoday01@gmail.com>
2024-03-08 11:55:25 +0530

    added game history

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-const StatusMessage = ({ winner, isXNext, squares }) => {
-  // eslint-disable-next-line react/prop-types
+const StatusMessage = ({ winner, gamingBoard }) => {
+  const { squares, isXNext } = gamingBoard;
   const noMovesLeft = squares.every(squareValue => squareValue !== null);
   const nextPlayer = isXNext ? 'X' : 'O';
 
