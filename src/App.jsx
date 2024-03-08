7451f2c31151efcd164d4d86da961199051daa0f
Tiruveedi Poornoday <tpoornoday01@gmail.com>
2024-03-08 16:29:48 +0530

    added title and added roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -77,7 +80,13 @@ function App() {
       >
         Start New Game
       </button>
-      <h2>Current game history</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current game history
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
